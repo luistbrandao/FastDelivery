4bf14a8f0f9f9e8d73709e0cbbe1a5763e50b6b7
luistbrandao <luisotbrandao@gmail.com>
2018-09-05 17:34:01 -0300

    Update cliente.java

--- NetBeansProjects/FastDelivery/src/fastdelivery/cliente.java
+++ NetBeansProjects/FastDelivery/src/fastdelivery/cliente.java
@@ -9,20 +9,29 @@ package fastdelivery;
  *
  * @author Thiago
  */
-public class cliente {
+public class cliente extends Endereco {
     
         private String cpf; // O CPF vai identificar o cliente [Não iremos utilizar senha e email]
         private String telefone;
         private String nome;
+        private Endereco endereco;
         
-        public cliente(String cpf,String telefone,String nome){ // Contrutor ele serve para quando cria o objeto criea automaticamente esses atriburos e tem que passar esses atributos por parametro
+        public cliente(String cpf,String telefone,String nome, Endereco endereco){ // Contrutor ele serve para quando cria o objeto criea automaticamente esses atriburos e tem que passar esses atributos por parametro
             this.cpf = cpf;
             this.telefone = telefone;
             this.nome = nome;
+            this.endereco = new Endereco();
         }
         
         //Aqui nos get e setters tem alguns que nao precisamos mas eu nao sei qual que precisa agente fala com TJ para tirar a dúvida
 
+   
+        
+        
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }    
+        
     public String getCpf() {
         return cpf;
     }
@@ -33,6 +42,18 @@ public class cliente {
 
     public String getNome() {
         return nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
 
